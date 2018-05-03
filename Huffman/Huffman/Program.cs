de5d8f150029f780a7a0d2fefdef3f2bd4caa74b
niclspschd <nicolas.paschoud@gmail.com>
2018-05-03 10:52:41 +0200

    Les pointeurs pour les arbres
    On fait du pointage pour l'arbre

--- Huffman/Huffman/Program.cs
+++ Huffman/Huffman/Program.cs
@@ -21,21 +21,25 @@ namespace Huffman
             return File.ReadAllText(chemin);
         }
 
-        public static Dictionary<char, double> TrieDico(string text)
+        public static Dictionary<char, double> CreateDico(string text)
         {
             Dictionary<char, double> dico = new Dictionary<char, double>();
 
+            //  Rajoute les lettres dans le dico
             foreach (char lettre in text)
             {
                 if (!dico.ContainsKey(lettre))
                     dico.Add(lettre, 1);
+                // Si la lettre existe deja augmente son nombre d'occurence
                 else
                     dico[lettre]++;
             }
 
+            //  Parcours le dico et fait des trucs bizzare
             for (int i = 0; i < dico.Count; i++)
             {
                 char lettre = dico.ElementAt(i).Key;
+                //  Remplace l'occurence par la probabilité
                 dico[lettre] = (dico[lettre] / text.Length);
             }
 
@@ -60,103 +64,132 @@ namespace Huffman
             return entropie;
         }
 
-        public struct TreeContent
+        //public static TreeContent[] CreateTree(Dictionary<char, double> dico)
+        //{
+        //    List<TreeContent> ListAInserer = new List<TreeContent>();
+        //    TreeContent[] HuffmanTree = new TreeContent[dico.Count];
+        //    foreach (char chara in dico.Keys)
+        //    {
+        //        ListAInserer.Add(new TreeContent() { Key = chara, Proba = dico[chara] });
+        //    }
+
+        //    int last = 0;
+        //    //  Creation de l'arbre Huffman
+        //    for (int i = 0; i < HuffmanTree.Length; i += 2)
+        //    {
+        //        // ListAInserer.Sort();
+
+        //        last = HuffmanTree.Length - 1 - i;
+        //        if (last != 0)
+        //        {
+        //            HuffmanTree[last] = ListAInserer[last];
+        //            HuffmanTree[last - 1] = ListAInserer[last - 1];
+
+        //            ListAInserer.Add(new TreeContent() { Key = ' ', Proba = (ListAInserer[last].Proba + ListAInserer[last - 1].Proba) });
+
+        //            ListAInserer.RemoveAt(ListAInserer.Count - 2);
+        //            ListAInserer.RemoveAt(ListAInserer.Count - 2);
+        //        }
+        //    }
+
+        //    return HuffmanTree;
+        //}
+        unsafe public struct Noeud
         {
-            public char Key { get; set; }
-            public double Proba { get; set; }
+            public double Proba;
+            public Noeud* ElementG;
+            public Noeud* ElementD;
+            public char Lettre;
         };
 
-        public static TreeContent[] CreateTree(Dictionary<char, double> dico)
+        public static Dictionary<char, double> TriDico(Dictionary<char, double> dico)
         {
-            List<TreeContent> ListAInserer = new List<TreeContent>();
-            TreeContent[] HuffmanTree = new TreeContent[dico.Count];
-            foreach (char chara in dico.Keys)
+            var sortedList = from pair in dico orderby pair.Value descending select pair;
+            Dictionary<char, double> dicoTrier = new Dictionary<char, double>();
+            foreach (KeyValuePair<char, double> lettre in sortedList)
             {
-                ListAInserer.Add(new TreeContent() { Key = chara, Proba = dico[chara] });
+                dicoTrier.Add(lettre.Key, lettre.Value);
             }
 
-            int last = 0;
-            //  Creation de l'arbre Huffman
-            for (int i = 0; i < HuffmanTree.Length; i += 2)
-            {
-                ListAInserer.Sort();
-
-                last = HuffmanTree.Length - 1 - i;
-                if (last != 0)
-                {
-                    HuffmanTree[last] = ListAInserer[last];
-                    HuffmanTree[last - 1] = ListAInserer[last - 1];
-
-                    ListAInserer.Add(new TreeContent() { Key = ' ', Proba = (ListAInserer[last].Proba + ListAInserer[last - 1].Proba) });
-
-                    ListAInserer.RemoveAt(ListAInserer.Count - 2);
-                    ListAInserer.RemoveAt(ListAInserer.Count - 2);
-                }
-            }
-
-            return HuffmanTree;
+            return dicoTrier;
         }
 
-        //public static List<TreeContent> TriParTas(List<TreeContent> list)
-        //{
-        //    TreeContent temp;
-        //    for (int i = 0; i < list.Count; i++)
-        //    {
-        //        list = Tas(list, list.Count - i);
-        //        temp = list[0];
-        //        list[0] = list[list.Count - 1 - i];
-        //        list[list.Count - 1 - i] = temp;
-        //    }
+        unsafe public static Noeud createNoeud(double proba, Noeud* SAG = null, Noeud* SAD = null, char key = '\0')
+        {
+            Noeud node = new Noeud { Proba = proba, ElementG = SAG, ElementD = SAD, Lettre = key };
+            return node;
+        }
 
-        //    return list;
-        //}
+        unsafe public static Noeud CreateTree(Dictionary<char, double> dico)
+        {
+            Noeud tree = new Noeud();
+            List<Noeud> arbre = new List<Noeud>();
+            Noeud n;
 
-        //public static List<TreeContent> Tas(List<TreeContent> list, int taille)
-        //{
-        //    for (int i = 1; i < taille; i++)
-        //    {
-        //        list = TriTasParents(list, i);
-        //    }
-        //    return list;
-        //}
+            foreach (char key in dico.Keys)
+            {
+                n = new Noeud { Proba = dico[key], ElementG = null, ElementD = null, Lettre = key };
+                arbre.Add(n);
+            }
 
-        //public static List<TreeContent> TriTasParents(List<TreeContent> list, int index)
-        //{
-        //    double x = index / 2;
-        //    int y = (int)Math.Ceiling(x);
-        //    if (y - 1 > 0)
-        //        if (list[index].Proba > list[y - 1].Proba)
-        //        {
-        //            TreeContent z = list[index];
-        //            list[index] = list[y - 1];
-        //            list[y - 1] = z;
-        //            if (y != 0)
-        //                TriTasParents(list, y);
-        //        }
-        //    return list;
-        //}
+            
+            /*Noeud temp;
+            Noeud connexion;
+            
+            while (dico != null) {
+                dico = TriDico(dico);
+                Noeud firstNode = createNoeud(dico[dico.Last().Key], null, null, dico.Last().Key);
+                dico.Remove(dico.Last().Key);
+                Noeud secondNode = createNoeud(dico[dico.Last().Key], null, null, dico.Last().Key);
+                dico.Remove(dico.Last().Key);
+                // Qu'est ce qu'il reste de la boucle d'avant ici ?
+                //  Juste une proba dans un dico
 
-        static void Main(string[] args)
+                double addition = firstNode.Proba + secondNode.Proba;
+                //Création de connexion
+                connexion = createNoeud(addition, &firstNode, &secondNode);
+                
+                if (dico.ContainsKey('\0')) {
+                    addition = tree.Proba + connexion.Proba;
+                    temp = createNoeud(addition, &tree, &connexion);
+                    tree = temp;
+                    dico.Remove('\0');
+                } else {
+                    tree = connexion;
+                }
+
+                dico.Add('\0', addition);
+
+            } // Suppression de connexion*/
+            return tree;
+        }
+
+        unsafe static void Main(string[] args)
         {
             Console.WriteLine("Veuillez entrez le nom du fichier à analyser");
             string fichier = "./TheSonnets.txt";//Console.ReadLine();
 
             string text = LireFichier(fichier);
             int longueurText = text.Length;
-            Dictionary<char, double> dico = TrieDico(text);
+            Dictionary<char, double> dico = CreateDico(text);
 
             AffichageDico(dico);
 
             double entropie = EntropieCalcul(dico, longueurText);
+            double redondance = Math.Log(dico.Count, 2) - entropie;
 
+            Console.WriteLine("\nRedondance (R=D-H) : " + redondance);
             Console.WriteLine("Entropie : " + entropie.ToString());
 
-            TreeContent[] myTab = CreateTree(dico);
 
-            foreach (var item in myTab)
-            {
-                Console.WriteLine("Key : " + item.Key + " Proba : " + item.Proba);
-            }
+            Noeud node = CreateTree(dico);
+            Console.WriteLine(node.Proba);
+            //TreeContent[] myTab = CreateTree(dico);
+
+            //foreach (var item in myTab)
+            //{
+            //    Console.WriteLine("Key : " + item.Key + " Proba : " + item.Proba);
+            //}
 
             Console.ReadKey();
 
